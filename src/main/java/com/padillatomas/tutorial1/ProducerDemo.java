ca8bd1e8766f8df4c759878bb33b99e294818232
Tomas Padilla <padillatomasagustin@gmail.com>
2022-07-27 08:41:48 +0200

    Develop - Starting a Producer

--- src/main/java/com/padillatomas/tutorial1/ProducerDemo.java
+++ src/main/java/com/padillatomas/tutorial1/ProducerDemo.java
@@ -1,7 +1,11 @@
 package com.padillatomas.tutorial1;
 
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        Create Producer Properties
+//        Create Producer
+//        Send Data
     }
 }
